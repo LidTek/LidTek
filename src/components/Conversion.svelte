5912d8a4c3cfb5db16e71f1760e39f4ec46e3e55
lidldev <ahmet.affan.ebcioglu@outlook.com>
2024-08-07 20:25:55 +0300

    style change

--- src/components/Conversion.svelte
+++ src/components/Conversion.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <section
-  class="py-14 md:py-20 flex flex-col gap-8 bg-[#181b34] text-white text-center items-center justify-center"
+  class="py-14 md:py-20 flex flex-col gap-8 bg-[rgb(150,150,200)] text-white text-center items-center justify-center"
 >
   <h4 class="text-xl sm:text-2xl md:text-3xl poppins">
     Be Social! <span class="poppins font-bold">Get Ready To Socialize!</span>
