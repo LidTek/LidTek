b41381b6dd60364259016ca0986d3f1e52ed3ba6
lidldev <ahmet.affan.ebcioglu@outlook.com>
2024-08-10 20:57:05 +0300

    tried to fix :(

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,16 @@
 <script>
-  import "../app.css";
-  import Footer from "../components/Footer.svelte";
-  import Header from "../components/Header.svelte";
+    import "../app.css";
+    import Footer from "../components/Footer.svelte";
+    import Header from "../components/Header.svelte";
+    import { openModal } from "../store";
+
+    let y;
+    $: outerHeight = 0;
+
+    function reroute(href) {
+        $openModal = false;
+        window.location.href = href;
+    }
 </script>
 
 <Header></Header>
